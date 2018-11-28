07b6e958eaad00eec50b97d9fe205c2e5996a4cb
jinx <jin.xu@oracle.com>
2018-11-28 12:51:32 +0800

    first commit

---
+++ example/example.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"context"
+
+	"github.com/rancher/kontainer-engine/service"
+	"github.com/rancher/kontainer-engine/store"
+	"github.com/rancher/types/apis/management.cattle.io/v3"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	time.Sleep(time.Second * 2)
+	credentialPath := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	data, err := ioutil.ReadFile(credentialPath)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	b := true
+	gkeSpec := &v3.GoogleKubernetesEngineConfig{
+		ProjectID:                 "rancher-dev",
+		Zone:                      "us-central1-a",
+		NodeCount:                 1,
+		EnableKubernetesDashboard: true,
+		EnableHTTPLoadBalancing:   &b,
+		ImageType:                 "ubuntu",
+		EnableLegacyAbac:          true,
+		Locations:                 []string{"us-central1-a", "us-central1-b"},
+		Credential:                string(data),
+	}
+	spec := v3.ClusterSpec{
+		GoogleKubernetesEngineConfig: gkeSpec,
+	}
+
+	// You should really implement your own store
+	store := store.CLIPersistStore{}
+	service := service.NewEngineService(store)
+
+	endpoint, token, cert, err := service.Create(context.Background(), "daishan-test", spec)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	fmt.Println(endpoint)
+	fmt.Println(token)
+	fmt.Println(cert)
+	err = service.Remove(context.Background(), "daishan-test", spec)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+}
---
+++ service/service_test.go
@@ -0,0 +1,73 @@
+package service
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/rancher/kontainer-engine/types"
+	"github.com/rancher/types/apis/management.cattle.io/v3"
+	"gopkg.in/check.v1"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) {
+	check.TestingT(t)
+}
+
+type StubTestSuite struct {
+}
+
+var _ = check.Suite(&StubTestSuite{})
+
+func (s *StubTestSuite) SetUpSuite(c *check.C) {
+}
+
+func (s *StubTestSuite) TestFlatten(c *check.C) {
+	config := v3.GoogleKubernetesEngineConfig{
+		ProjectID:  "test",
+		Zone:       "test",
+		DiskSizeGb: 50,
+		Labels: map[string]string{
+			"foo": "bar",
+		},
+		EnableAlphaFeature: true,
+	}
+	config.MasterVersion = "1.7.1"
+	config.NodeVersion = "1.7.1"
+	config.NodeCount = 3
+	opts, err := toMap(config, "json")
+	if err != nil {
+		c.Fatal(err)
+	}
+	driverOptions := types.DriverOptions{
+		BoolOptions:        make(map[string]bool),
+		StringOptions:      make(map[string]string),
+		IntOptions:         make(map[string]int64),
+		StringSliceOptions: make(map[string]*types.StringSlice),
+	}
+	flatten(opts, &driverOptions)
+	fmt.Println(driverOptions)
+	boolResult := map[string]bool{
+		"enableAlphaFeature": true,
+	}
+	stringResult := map[string]string{
+		"projectId":         "test",
+		"zone":              "test",
+		"masterVersion":     "1.7.1",
+		"maintenanceWindow": "",
+		"nodeVersion":       "1.7.1",
+	}
+	intResult := map[string]int64{
+		"diskSizeGb": 50,
+		"nodeCount":  3,
+	}
+	stringSliceResult := map[string]types.StringSlice{
+		"labels": {
+			Value: []string{"foo=bar"},
+		},
+	}
+	c.Assert(driverOptions.BoolOptions, check.DeepEquals, boolResult)
+	c.Assert(driverOptions.IntOptions, check.DeepEquals, intResult)
+	c.Assert(driverOptions.StringOptions, check.DeepEquals, stringResult)
+	c.Assert(driverOptions.StringSliceOptions["labels"].Value, check.DeepEquals, stringSliceResult["labels"].Value)
+}
